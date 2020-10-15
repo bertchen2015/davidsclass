030e795d7fd9e32df03af796dea85b9836f094f3
Albert Chen <63614559+bertchen2015@users.noreply.github.com>
2020-10-14 22:39:19 -0700

    Add files via upload

---
+++ Javascript/node-files/readFile.js
@@ -0,0 +1,19 @@
+const fs = require('fs').promises;
+
+async function openFile() {
+  try {
+    const csvHeaders = 'name,quantity,price'
+    await fs.writeFile('groceries.csv', csvHeaders);
+  } catch (error) {
+    console.error(`Got an error trying to write to a file: ${error.message}`);
+  }
+}
+
+async function addGroceryItem(name, quantity, price) {
+  try {
+    const csvLine = `\n${name},${quantity},${price}`
+    await fs.writeFile('groceries.csv', csvLine, { flag: 'a' });
+  } catch (error) {
+    console.error(`Got an error trying to write to a file: ${error.message}`);
+  }
+}
